0110e0856d523ae166054771d740017cc4fef5ac
7DR3am <glazov.egor2007@gmail.com>
2025-09-21 14:21:17 +0300

    Change operation to subtraction via GitHub

--- program.py
+++ program.py
@@ -2,5 +2,5 @@ print("Hello, World!")
 
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
-result = num1 + num2
+result = num1 - num2
 print(f"The sum is: {result}")
